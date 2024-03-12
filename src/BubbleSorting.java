ab0dbfdc98a2c9e093e3b0a150eb2b77f5f2ee3a
Vitalii Sukach <sukach.v.v@gmail.com>
2024-03-12 16:29:19 +0200

    Add bubble sorting

--- src/BubbleSorting.java
+++ src/BubbleSorting.java
@@ -13,7 +13,15 @@ public class BubbleSorting {
 
     // Bubble sort algorithm
     public static void bubbleSort(int[] arr) {
-
+        for (var i = 0; i < arr.length; i++ ) {
+            for (var j = 1; j < arr.length - i; j++) {
+                if (arr[j - 1] > arr[j]) {
+                    var temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                }
+            }
+        }
     }
 
     // Utility method to print an array
