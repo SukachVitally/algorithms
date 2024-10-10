b80a76f973f4c519a31679e08a149f18f460bc80
Vitalii Sukach <sukach.v.v@gmail.com>
2024-10-10 17:23:24 +0300

    tmp

--- src/concurrency_test/MergeSortingTest.java
+++ src/concurrency_test/MergeSortingTest.java
@@ -123,73 +123,73 @@ class ParallelMergeSorter2 {
     }
 }
 
-//class ParallelMergeSorter {
-//
-//    private int[] array;
-//
-//    public ParallelMergeSorter(int[] array) {
-//        this.array = array;
-//    }
-//
-//    /* returns sorted array */
-//    public int[] sort() {
-//        int numWorkers = Runtime.getRuntime().availableProcessors();
-//        ForkJoinPool pool = new ForkJoinPool(numWorkers);
-//        pool.invoke(new ParallelWorker(0,array.length-1));
-//        return array;
-//    }
-//
-//    /* worker that gets called recursively */
-//    private class ParallelWorker extends RecursiveAction {
-//
-//        private int left, right;
-//
-//        public ParallelWorker(int left, int right) {
-//            this.left = left;
-//            this.right = right;
-//        }
-//
-//        protected void compute() {
-//            if (left < right) {
-//                int mid = (left + right) / 2; // find the middle point
-//                ParallelWorker leftWorker = new ParallelWorker(left, mid);
-//                ParallelWorker rightWorker = new ParallelWorker(mid+1, right);
-//                invokeAll(leftWorker, rightWorker);
-//                merge(left, mid, right); // merge the two sorted halves
-//            }
-//        }
-//    }
-//
-//    /* helper method to merge two sorted subarrays array[l..m] and array[m+1..r] into array */
-//    private void merge(int left, int mid, int right) {
-//        // copy data to temp subarrays to be merged
-//        int leftTempArray[] = Arrays.copyOfRange(array, left, mid+1);
-//        int rightTempArray[] = Arrays.copyOfRange(array, mid+1, right+1);
-//
-//        // initial indexes for left, right, and merged subarrays
-//        int leftTempIndex=0, rightTempIndex=0, mergeIndex=left;
-//
-//        // merge temp arrays into original
-//        while (leftTempIndex < mid - left + 1 || rightTempIndex < right - mid) {
-//            if (leftTempIndex < mid - left + 1 && rightTempIndex < right - mid) {
-//                if (leftTempArray[leftTempIndex] <= rightTempArray[rightTempIndex]) {
-//                    array[mergeIndex ] = leftTempArray[leftTempIndex];
-//                    leftTempIndex++;
-//                } else {
-//                    array[mergeIndex ] = rightTempArray[rightTempIndex];
-//                    rightTempIndex++;
-//                }
-//            } else if (leftTempIndex < mid - left + 1) { // copy any remaining on left side
-//                array[mergeIndex ] = leftTempArray[leftTempIndex];
-//                leftTempIndex++;
-//            } else if (rightTempIndex < right - mid) { // copy any remaining on right side
-//                array[mergeIndex ] = rightTempArray[rightTempIndex];
-//                rightTempIndex++;
-//            }
-//            mergeIndex++;
-//        }
-//    }
-//}
+class ParallelMergeSorter {
+
+    private int[] array;
+
+    public ParallelMergeSorter(int[] array) {
+        this.array = array;
+    }
+
+    /* returns sorted array */
+    public int[] sort() {
+        int numWorkers = Runtime.getRuntime().availableProcessors();
+        ForkJoinPool pool = new ForkJoinPool(numWorkers);
+        pool.invoke(new ParallelWorker(0,array.length-1));
+        return array;
+    }
+
+    /* worker that gets called recursively */
+    private class ParallelWorker extends RecursiveAction {
+
+        private int left, right;
+
+        public ParallelWorker(int left, int right) {
+            this.left = left;
+            this.right = right;
+        }
+
+        protected void compute() {
+            if (left < right) {
+                int mid = (left + right) / 2; // find the middle point
+                ParallelWorker leftWorker = new ParallelWorker(left, mid);
+                ParallelWorker rightWorker = new ParallelWorker(mid+1, right);
+                invokeAll(leftWorker, rightWorker);
+                merge(left, mid, right); // merge the two sorted halves
+            }
+        }
+    }
+
+    /* helper method to merge two sorted subarrays array[l..m] and array[m+1..r] into array */
+    private void merge(int left, int mid, int right) {
+        // copy data to temp subarrays to be merged
+        int leftTempArray[] = Arrays.copyOfRange(array, left, mid+1);
+        int rightTempArray[] = Arrays.copyOfRange(array, mid+1, right+1);
+
+        // initial indexes for left, right, and merged subarrays
+        int leftTempIndex=0, rightTempIndex=0, mergeIndex=left;
+
+        // merge temp arrays into original
+        while (leftTempIndex < mid - left + 1 || rightTempIndex < right - mid) {
+            if (leftTempIndex < mid - left + 1 && rightTempIndex < right - mid) {
+                if (leftTempArray[leftTempIndex] <= rightTempArray[rightTempIndex]) {
+                    array[mergeIndex ] = leftTempArray[leftTempIndex];
+                    leftTempIndex++;
+                } else {
+                    array[mergeIndex ] = rightTempArray[rightTempIndex];
+                    rightTempIndex++;
+                }
+            } else if (leftTempIndex < mid - left + 1) { // copy any remaining on left side
+                array[mergeIndex ] = leftTempArray[leftTempIndex];
+                leftTempIndex++;
+            } else if (rightTempIndex < right - mid) { // copy any remaining on right side
+                array[mergeIndex ] = rightTempArray[rightTempIndex];
+                rightTempIndex++;
+            }
+            mergeIndex++;
+        }
+    }
+}
 
 public class MergeSortingTest {
 
@@ -206,7 +206,7 @@ public class MergeSortingTest {
     /* evaluate performance of sequential and parallel implementations */
     public static void main(String[] args) {
         final int NUM_EVAL_RUNS = 5;
-        final int[] input = generateRandomArray(100_000);
+        final int[] input = generateRandomArray(1000_000);
 
         System.out.println("Evaluating Sequential Implementation...");
         SequentialMergeSorter sms = new SequentialMergeSorter(Arrays.copyOf(input, input.length));
@@ -221,26 +221,26 @@ public class MergeSortingTest {
         sequentialTime /= NUM_EVAL_RUNS;
 
         System.out.println("Evaluating Parallel Implementation...");
-        ParallelMergeSorter2 pms = new ParallelMergeSorter2(Arrays.copyOf(input, input.length));
-        int[] parallelResult = pms.sort();
-        double parallelTime = 0;
-        for(int i=0; i<NUM_EVAL_RUNS; i++) {
-            pms = new ParallelMergeSorter2(Arrays.copyOf(input, input.length));
-            long start = System.currentTimeMillis();
-            pms.sort();
-            parallelTime += System.currentTimeMillis() - start;
-        }
-
-//        ParallelMergeSorter pms = new ParallelMergeSorter(Arrays.copyOf(input, input.length));
+//        ParallelMergeSorter2 pms = new ParallelMergeSorter2(Arrays.copyOf(input, input.length));
 //        int[] parallelResult = pms.sort();
 //        double parallelTime = 0;
 //        for(int i=0; i<NUM_EVAL_RUNS; i++) {
-//            pms = new ParallelMergeSorter(Arrays.copyOf(input, input.length));
+//            pms = new ParallelMergeSorter2(Arrays.copyOf(input, input.length));
 //            long start = System.currentTimeMillis();
 //            pms.sort();
 //            parallelTime += System.currentTimeMillis() - start;
 //        }
-//        parallelTime /= NUM_EVAL_RUNS;
+
+        ParallelMergeSorter pms = new ParallelMergeSorter(Arrays.copyOf(input, input.length));
+        int[] parallelResult = pms.sort();
+        double parallelTime = 0;
+        for(int i=0; i<NUM_EVAL_RUNS; i++) {
+            pms = new ParallelMergeSorter(Arrays.copyOf(input, input.length));
+            long start = System.currentTimeMillis();
+            pms.sort();
+            parallelTime += System.currentTimeMillis() - start;
+        }
+        parallelTime /= NUM_EVAL_RUNS;
 
         // display sequential and parallel results for comparison
         if (!Arrays.equals(sequentialResult, parallelResult))
